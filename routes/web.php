f89fbd766415010b2857ea16c351b473823deebe
Narzullo <olluzran@gmail.com>
2021-02-11 10:08:20 +0300

    view product details

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\DetailsComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,7 @@ Route::get('/shop', ShopComponent::class);
 Route::get('/cart', CartComponent::class);
 
 Route::get('/checkout', CheckoutComponent::class);
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
