9991da4b763e088cc3d3131d0f04eee51ecbbf19
Narzullo <olluzran@gmail.com>
2021-02-13 23:06:43 +0300

    ui of add new category is done

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\DetailsComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -53,5 +54,6 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 	Route::get('admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
 	Route::get('/admin/category',AdminCategoryComponent::class)->name('admin.categories');
+	Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
 
 });
