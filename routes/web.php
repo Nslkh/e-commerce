ea72b75bbbdb36e3c8b7d38a988de77c16ef100b
Narzullo <olluzran@gmail.com>
2021-02-14 17:20:14 +0300

    Add New Product feature added, started implementing it

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\DetailsComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -59,5 +60,6 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 	Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
 	Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
 	Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
+	Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
 
 });
