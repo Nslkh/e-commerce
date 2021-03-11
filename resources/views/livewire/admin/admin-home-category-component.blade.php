f04e463c6191683b338305ada359e4ada240fcac
Nslkh <olluzran@gmail.com>
2021-03-11 12:44:54 +0500

    Manage Home Categories  Forms created

--- resources/views/livewire/admin/admin-home-category-component.blade.php
+++ resources/views/livewire/admin/admin-home-category-component.blade.php
@@ -1,3 +1,40 @@
 <div>
-    {{-- Stop trying to control. --}}
+   <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Manage Home Categories
+                    </div>
+                    <div class="panel-body">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-md-4 control-lavel" >Chose Categories</label>
+                                <div class="col-md-4">
+                                    <select class="sel_categories form-control" name="categories[]" multiple="multiple"></select>
+                                    @foreach ($categories as $category)
+                                        <option value="{{$category->id}}">{{$category->name}}</option>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-lavel" >No of Products</label>
+                                <div class="col-md-4">
+                                   <input type="text" class="form-control input-md">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-lavel" ></label>
+                                <div class="col-md-4">
+                                  <button type="submit" class="btn btn-primary">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </div>
 </div>
