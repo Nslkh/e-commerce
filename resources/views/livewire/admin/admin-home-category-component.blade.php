8c1bc27c9860d256aba66e5d19fe3f8fee0dac19
Nslkh <olluzran@gmail.com>
2021-03-11 22:19:19 +0500

    changing the no of products on categories

--- resources/views/livewire/admin/admin-home-category-component.blade.php
+++ resources/views/livewire/admin/admin-home-category-component.blade.php
@@ -7,11 +7,12 @@
                         Manage Home Categories
                     </div>
                     <div class="panel-body">
-                        <form class="form-horizontal">
+
+                        <form class="form-horizontal" wire:submit.prevent="updateHomeCategory">
                             <div class="form-group">
                                 <label class="col-md-4 control-lavel" >Chose Categories</label>
-                                <div class="col-md-4">
-                                    <select class="sel_categories form-control" name="categories[]" multiple="multiple">
+                                <div class="col-md-4" wire:ignore>
+                                    <select class="sel_categories form-control" name="categories[]" multiple="multiple" wire:model="selected_categories">
                                     @foreach ($categories as $category)
                                         <option value="{{$category->id}}">{{$category->name}}</option>
                                     @endforeach
@@ -22,7 +23,7 @@
                             <div class="form-group">
                                 <label class="col-md-4 control-lavel" >No of Products</label>
                                 <div class="col-md-4">
-                                   <input type="text" class="form-control input-md">
+                                   <input type="text" class="form-control input-md" wire:model="numberofproducts">
                                 </div>
                             </div>
 
@@ -40,10 +41,15 @@
    </div>
 </div>
 
+
 @push('scripts')
     <script>
         $(document).ready(function(){
             $('.sel_categories').select2();
+            $('.sel_categories').on('change',function(e){
+                var data = $('.sel_categories').select2("val");
+                @this.set('selected_categories',data);
+            });
         });
     </script>
 @endpush
