d196be23f28b2d48d7349972d275ab12db812e08
Nslkh <olluzran@gmail.com>
2021-03-12 20:48:52 +0500

    admin add category validation for name / slug

--- app/Http/Livewire/Admin/AdminAddCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminAddCategoryComponent.php
@@ -2,31 +2,43 @@
 
 namespace App\Http\Livewire\Admin;
 
-use App\Models\Category; 
+use App\Models\Category;
 use Livewire\Component;
 use Illuminate\Support\Str;
 
 class AdminAddCategoryComponent extends Component
 {
-	public $name; 
-	public $slug; 
+    public $name;
+    public $slug;
 
-	public function generateslug()
-	{
-		$this->slug = Str::slug($this->name); 
-	}
+    public function generateslug()
+    {
+        $this->slug = Str::slug($this->name);
+    }
 
-	public function storeCategory()
-	{
-		$category = new Category(); 
-		$category->name = $this->name;
-		$category->slug = $this->slug;
-		$category->save();
-		session()->flash('message','Category has been created Successfully!'); 
-	}
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'name' => 'required',
+            'slug' => 'required|unique:categories'
+        ]);
+    }
+
+    public function storeCategory()
+    {
+        $this->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:categories'
+        ]);
+        $category = new Category();
+        $category->name = $this->name;
+        $category->slug = $this->slug;
+        $category->save();
+        session()->flash('message', 'Category has been created Successfully!');
+    }
 
     public function render()
     {
-        return view('livewire.admin.admin-add-category-component')->layout('layouts.base');  
+        return view('livewire.admin.admin-add-category-component')->layout('layouts.base');
     }
 }
