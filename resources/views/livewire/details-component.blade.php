790dbfd6a88c3e612d10c82e8cd22d5a9ec8ea03
Nslkh <olluzran@gmail.com>
2021-03-13 23:46:41 +0500

    Showing  On Sale  In Product Details

--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -1,4 +1,13 @@
 <main id="main" class="main-site">
+	<style>
+		.regprice{
+			font-weight: 300px;
+			font-size: 13px !important;
+			color: #aaaaaa !important;
+			text-decoration: line-through;
+			padding-left: 10px; 
+		}
+	</style>
 	<div class="container">
 		<div class="wrap-breadcrumb">
 			<ul>
@@ -34,7 +43,14 @@
                         <div class="wrap-social">
                         	<a class="link-socail" href="#"><img src="{{ asset('assets/images/social-list.png') }}"></a>
                         </div>
-                        <div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
+						@if($product->sale_price > 0)
+							<div class="wrap-price">
+								<span class="product-price">${{$product->sale_price}}</span>
+								<del><span class="product-price regprice">${{$product->regular_price}}</span></del>
+							</div>
+						@else
+                        	<div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
+						@endif
                         <div class="stock-info in-stock">
                             <p class="availability">Availability: <b>{{$product->stock_status}}</b></p>
                         </div>
